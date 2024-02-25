e5ab8db19d13bd48ee4afc6970586bf137b3d3fd
Charles <charles@klei.vip>
2024-02-26 01:01:10 +0800

    0.0.1 初始化，明天再慢慢写了（

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
+import puppeteer from "koishi-plugin-puppeteer";
 
-export const name = 'oni-wiki-qq'
+export const name = "oni-wiki-qq";
+export const inject = ["puppeteer"];
+export const usage = `
+  - 0.0.1 初始化，明天再慢慢写了（
+`;
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context) {
-  // write your plugin here
-}
+export function apply(ctx: Context) {}
