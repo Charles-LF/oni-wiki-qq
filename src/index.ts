9e8c4cf97d4135a556248b149fafb8f6d6950bc6
Charles Lf <charles@klei.vip>
2025-09-20 10:52:16 +0800

    移除不必要的功能

--- src/index.ts
+++ src/index.ts
@@ -23,19 +23,14 @@
 //          本来无BUG            何必常修改
 //                  佛曰: 能跑就行
 
-import { Context, Schema, Logger, Time, $ } from "koishi";
+import { Context, Schema, Logger } from "koishi";
+import { Mwn } from 'mwn'
 
 export const name = "oni-wiki-qq";
 
 export const usage = `
+  - 0.4.6 移除没必要的功能
   - 0.4.5 检测教程页面
-  - 0.4.4 加入用户页面
-  - 0.4.3 收集搜索的词汇频率
-  - 0.4.2 修改/ 的使用方式,改为 >
-  - 0.4.1 增加/的处理
-  - 0.4.0 移除MWN.将查询处理改至查询数据库
-  - 0.3.0 移除耗内存的截图部分,使用镜像站点网页
-  - 0.2.0 尝试添加 MWN 库
 `;
 
 export const inject = ["database"];
@@ -44,7 +39,6 @@ export const inject = ["database"];
 declare module "koishi" {
   interface Tables {
     wikipages: WikiPages; // 全部页面
-    searchpages: searchPages; // 搜索词
   }
 }
 
@@ -53,18 +47,14 @@ export interface WikiPages {
   title: string;
 }
 
-export interface searchPages {
-  id: number;
-  title: string;
-  time: string;
-}
-
 // 配置项
 export interface Config {
-  SESSDATA: string;
+  bot_username: string;
+  bot_password: string;
 }
 export const Config: Schema<Config> = Schema.object({
-  SESSDATA: Schema.string().description("SESSDATA"),
+  bot_username: Schema.string().description("机器人用户名"),
+  bot_password: Schema.string().description("机器人密码"),
 });
 
 export function apply(ctx: Context, config: Config) {
@@ -74,18 +64,6 @@ export function apply(ctx: Context, config: Config) {
     id: "integer",
     title: "string",
   });
-  ctx.model.extend(
-    "searchpages",
-    {
-      id: "unsigned",
-      title: "string",
-      time: "string",
-    },
-    {
-      primary: "id",
-      autoInc: true,
-    }
-  );
 
   // 注册指令
   ctx
@@ -95,77 +73,53 @@ export function apply(ctx: Context, config: Config) {
       if (/教程/.test(itemName)) {
         return `请点击链接前往站点查看:\n原站点:  http://oni.wiki/${encodeURI(
           `教程`
-        )}\n镜像站:  https://klei.vip/oni/usiz6d/${encodeURI(`教程`)}`;
+        )}\n镜像站:  http://klei.vip/oni/usiz6d/${encodeURI(`教程`)}`;
       }
-      await ctx.database.create("searchpages", {
-        title: itemName,
-        time: Time.template("yyyy-MM-dd hh:mm:ss", new Date()),
-      });
       const res = await ctx.database.get("wikipages", {
         title: [`${itemName}`],
       });
-      logger.info(res);
       if (res.length == 0) {
         return `在Wiki里没找到,如有需要,请按照游戏内名称重新发起查询....`;
       }
       return `请点击链接前往站点查看:\n原站点:  http://oni.wiki/${encodeURI(
         itemName
-      )}\n镜像站:  https://klei.vip/oni/usiz6d/${encodeURI(itemName)}`;
+      )}\n镜像站:  http://klei.vip/oni/usiz6d/${encodeURI(itemName)}`;
     });
 
-  ctx
-    .command("update", "更新本地页面缓存", { authority: 2 })
-    .action(async () => {
-      const headers = {
-        "Content-Type": "application/json",
-        "user-agent": "Charles'queryBot",
-        Cookie: `SESSDATA=${config.SESSDATA}`,
-      };
-
-      const url = `https://wiki.biligame.com/oni/api.php?action=query&list=allpages&apnamespace=0&aplimit=5000&format=json`;
-
-      return await ctx.http
-        .get(url, {
-          headers: headers,
-        })
-        .then((res) => {
-          console.log(res["query"]["allpages"]);
-          res["query"]["allpages"].forEach(async (element) => {
-            console.log(element.title);
-            await ctx.database.upsert("wikipages", () => [
-              { id: element.pageid, title: element.title },
-            ]);
-          });
-          return `更新已完成,已尝试写入数据库}`;
-        })
-        .catch((err) => {
-          console.log(err);
-          return `出现了一点点问题`;
+  ctx.command("update", "更新本地页面缓存", { authority: 2 }).action(async ({ session }) => {
+    const bot = new Mwn({
+      apiUrl: 'https://oxygennotincluded.wiki.gg/zh/api.php',
+      username: config.bot_username,
+      password: config.bot_password,
+      userAgent: 'Charles`Bot/2.1 Charles@klei.vip',
+      defaultParams: {
+        assert: 'user'
+      }
+    });
+    bot.login().then(async () => {
+      logger.info('登录成功');
+      bot.request({
+        action: 'query',
+        list: 'allpages',
+        format: 'json',
+        aplimit: 'max'
+      }).then((res) => {
+        logger.info('查询成功');
+        const pages = res.query.allpages;
+        pages.forEach((page) => {
+          ctx.database.upsert('wikipages', () => [
+            { id: page.pageid, title: page.title },
+          ]);
         });
-    });
+        session.send(`检索到 ${pages.length} 个页面，已尝试更新至数据库`);
+        logger.info(`检索到 ${pages.length} 个页面，已尝试更新至数据库`);
 
-  ctx
-    .command("doc", "大叔的文档链接")
-    .alias("/大叔文档")
-    .action(async () => {
-      return `大叔的文档链接:\n https://klei.vip/oni/926f8b`;
+      }).catch((err) => {
+        logger.error('查询失败', err);
+      });
+
+    }).catch((err) => {
+      logger.error('登录失败', err);
     });
-  ctx
-    .command("RocketCalculator", "火箭计算器")
-    .alias("火箭计算器")
-    .action(async () => {
-      return `火箭计算器链接:\n https://klei.vip/oni/t93o56`;
-    });
-  // ctx
-  //   .command("frequency", "获取本月搜索次数最高的10位")
-  //   .alias("搜索频率")
-  //   .action(async ({ session }) => {
-  //     let now = Time.template("yyyy-MM-dd hh:mm:ss", new Date());
-  //     let a = await ctx.database
-  //       .select("searchpages")
-  //       .where((row) => $.gt(row.time, now))
-  //       .orderBy((row) => row.id)
-  //       .execute();
-  //     console.log(a);
-  //   });
+  })
 }
